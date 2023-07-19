3e7b67ce7e4e80b6c2ce97728bc0aa241e445928
JATothrim <jarmo.tiitto@gmail.com>
2023-07-19 20:18:30 +0300

    Add build time configure options.
    - Current `git rev-list -n1 HEAD`, used compiler and build type
      and critical settings are embedded into the cubes binary.
    - `cubes -v` prints now how it was built.
    - CUBES_MAX_N constant now available from "config.hpp"
    - New options can be added into "config.hpp.in"
    - Add anti-goof measure for read-only config.hpp

--- cpp/program.cpp
+++ cpp/program.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "cmdparser.hpp"
+#include "config.hpp"
 #include "cubes.hpp"
 
 void configure_arguments(cli::Parser& parser) {
@@ -8,12 +9,17 @@ void configure_arguments(cli::Parser& parser) {
     parser.set_optional<int>("t", "threads", 1, "the number of threads to use while generating");
     parser.set_optional<bool>("c", "use_cache", false, "whether to load cache files");
     parser.set_optional<bool>("w", "write_cache", false, "wheather to save cache files");
+    parser.set_optional<bool>("v", "version", false, "print build version info");
 }
 
 int main(int argc, char** argv) {
     cli::Parser parser(argc, argv);
     configure_arguments(parser);
     parser.run_and_exit_if_error();
+
+    if (parser.get<bool>("v")) {
+        std::printf("Built with %s, %s, %s CUBES_MAX_N:%d\n", CONFIG_VERSION, CONFIG_BUILDTYPE, CONFIG_COMPILERID, CUBES_MAX_N);
+    }
     gen(parser.get<int>("n"), parser.get<int>("t"), parser.get<bool>("c"), parser.get<bool>("w"));
     return 0;
 }
